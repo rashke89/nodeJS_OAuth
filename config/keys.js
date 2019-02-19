2f50555a1cb47e14a04578408f3785f6f6aee84c
Rados <rashke89@gmail.com>
2019-02-19 22:43:40 +0100

    added key.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
 module.exports = {
     google: {
-        clientID: '43440266636-fc98phprrcbqss7aea0unadkvaoe9eq0.apps.googleusercontent.com',
-        clientSecret: 'jnWirq0KJ2yMpNNTAR9O0NK5'
+        CLIENT_ID: '43440266636-fc98phprrcbqss7aea0unadkvaoe9eq0.apps.googleusercontent.com',
+        CLIENT_SECRET: 'jnWirq0KJ2yMpNNTAR9O0NK5'
+    },
+    mongoDB: {
+        dbURI: 'mongodb://nodeninja:nodeninja89@ds343985.mlab.com:43985/oauth_db'
     }
 }

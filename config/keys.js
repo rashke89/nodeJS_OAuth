77557ed4ae84fbf63c154f716cac1d098b27a125
Rados Mijatovic <rashke89@gmail.com>
2019-02-19 22:45:41 +0100

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    google: {
-        CLIENT_ID: '43440266636-fc98phprrcbqss7aea0unadkvaoe9eq0.apps.googleusercontent.com',
-        CLIENT_SECRET: 'jnWirq0KJ2yMpNNTAR9O0NK5'
-    },
-    mongoDB: {
-        dbURI: 'mongodb://nodeninja:nodeninja89@ds343985.mlab.com:43985/oauth_db'
-    }
-}
